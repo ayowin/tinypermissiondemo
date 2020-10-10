0c327583e24ebc56e6a4f3ed31a9f56555e655f2
OuyangWei <18344175265@126.com>
2020-10-10 10:04:17 +0800

    add ConfigurationInterface for decouple that parsing permissions

--- src/main/java/com/ouyangwei/tinypermissiondemo/ApiPermissionsUtil.java
+++ src/main/java/com/ouyangwei/tinypermissiondemo/ApiPermissionsUtil.java
@@ -3,19 +3,23 @@ package com.ouyangwei.tinypermissiondemo;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 @Component
 public class ApiPermissionsUtil {
 
-    private Map<String,String[]> permissionsMap = null;
+    private Map<String, Set<String>> permissionsMap = null;
 
     ApiPermissionsUtil(){
         permissionsMap = new HashMap<>();
-        permissionsMap.put("api_permission.dynamic_permission",new String[]{"manage"});
+        Set<String> set1 = new HashSet<>();
+        set1.add("manage");
+        permissionsMap.put("api_permission.dynamic_permission",set1);
     }
 
-    public String[] getPermissions(String dynamic){
+    public Set<String> getPermissions(String dynamic){
         return permissionsMap.get(dynamic);
     }
 
--- src/main/java/com/ouyangwei/tinypermissiondemo/configs/TinyPermissionConfiguration.java
+++ src/main/java/com/ouyangwei/tinypermissiondemo/configs/TinyPermissionConfiguration.java
@@ -4,6 +4,7 @@ import com.ouyangwei.tinypermissiondemo.ApiPermissionsUtil;
 import com.ouyangwei.tinypermissiondemo.UserUtil;
 import com.ouyangwei.tinypermissiondemo.annotations.TinyPermission;
 import com.ouyangwei.tinypermissiondemo.entities.User;
+import com.ouyangwei.tinypermissiondemo.interfaces.ConfigurationInterface;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
@@ -17,8 +18,38 @@ import java.util.*;
 @Aspect
 @Component
 public class TinyPermissionConfiguration {
+
+    private ConfigurationInterface configurationInterface = null;
+
+    TinyPermissionConfiguration(){
+        configurationInterface = new ConfigurationInterface() {
+            @Override
+            public Set<String> getDynamicPermissions(String dynamic) {
+                Set<String> set = apiPermissionsUtil.getPermissions(dynamic);
+                return set;
+            }
+
+            @Override
+            public Set<String> getUserPermissions(Object[] args) {
+                Set<String> set = new HashSet<>();
+                if(args[0] instanceof String){
+                    String token = (String)args[0];
+                    User user = userUtil.getUserByToken(token);
+                    String[] permissions = user.getRoles().split(",");
+                    if(permissions != null){
+                        for (String permission : permissions){
+                            set.add(permission);
+                        }
+                    }
+                }
+                return set;
+            }
+        };
+    }
+
     @Autowired
     UserUtil userUtil;
+
     @Autowired
     ApiPermissionsUtil apiPermissionsUtil;
 
@@ -55,32 +86,31 @@ public class TinyPermissionConfiguration {
         String dynamic = tinyPermission.dynamic();
 //        System.out.println(dynamic);
         if(dynamic != null && !value.equals("")){
-            String[] permissions = apiPermissionsUtil.getPermissions(dynamic);
-            if(permissions != null){
-                for (String permission : permissions){
-                    permissionSet.add(permission.trim());
+            if(configurationInterface != null){
+                Set<String> dynamicPermissionSet = configurationInterface.getDynamicPermissions(dynamic);
+                if(dynamicPermissionSet != null){
+                    for (String permission : dynamicPermissionSet){
+                        permissionSet.add(permission.trim());
+                    }
                 }
             }
         }
 
         Object[] args = joinPoint.getArgs();
         if(args != null && args.length > 0){
-            /* 第一个参数是token */
-            if(args[0] instanceof String){
-                String token = (String)args[0];
-                User user = userUtil.getUserByToken(token);
-                if(user != null){
+            if(configurationInterface != null){
+                Set<String> userPermissionSet = configurationInterface.getUserPermissions(args);
+                if(userPermissionSet != null){
                     for (String permission : permissionSet){
-                        if(user.getRoles().contains(permission)){
+                        if(userPermissionSet.contains(permission)){
                             return joinPoint.proceed();
                         }
                     }
-                    throw new Exception("user is not permited");
                 }
-                throw new Exception("user not exist");
+                throw new Exception("user is not permited or user not exist.");
             }
         }
-        throw new Exception("token not found");
+        throw new Exception("unless one parameters for ConfigurationInterface to parse user's permissions.");
     }
 
     @After("annotationPointcut()")
