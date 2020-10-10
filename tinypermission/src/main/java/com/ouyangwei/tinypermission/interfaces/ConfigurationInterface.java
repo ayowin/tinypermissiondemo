01a987b5a92af0822f1c3628c2cfa48820d51370
OuyangWei <18344175265@126.com>
2020-10-10 15:42:16 +0800

    add throw Throwable for interface

--- tinypermission/src/main/java/com/ouyangwei/tinypermission/interfaces/ConfigurationInterface.java
+++ tinypermission/src/main/java/com/ouyangwei/tinypermission/interfaces/ConfigurationInterface.java
@@ -3,6 +3,6 @@ package com.ouyangwei.tinypermission.interfaces;
 import java.util.Set;
 
 public interface ConfigurationInterface {
-    Set<String> getDynamicPermissions(String dynamic);
-    Set<String> getUserPermissions(Object[] args);
+    Set<String> getDynamicPermissions(String dynamic) throws Throwable;
+    Set<String> getUserPermissions(Object[] args) throws Throwable;
 }
