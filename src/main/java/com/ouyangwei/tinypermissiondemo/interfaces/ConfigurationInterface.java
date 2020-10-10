c39b5239018586c80ecf4458d5f4b54c2c4cecc1
OuyangWei <18344175265@126.com>
2020-10-10 10:22:47 +0800

    update project structure that tinypermission for decouple reuseable code and demo for tutorials

--- src/main/java/com/ouyangwei/tinypermissiondemo/interfaces/ConfigurationInterface.java
+++
@@ -1,8 +0,0 @@
-package com.ouyangwei.tinypermissiondemo.interfaces;
-
-import java.util.Set;
-
-public interface ConfigurationInterface {
-    Set<String> getDynamicPermissions(String dynamic);
-    Set<String> getUserPermissions(Object[] args);
-}
